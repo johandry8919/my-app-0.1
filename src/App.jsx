e6b6baca4263c9c1e2d94da57171211df36cbcef
johandry8919 <rojas.rojas440@gmail.com>
2023-04-16 13:00:12 -0400

    comit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import {  Routes, Route } from 'react-router-dom';
-import Home from './component/Home';
-import About from './component/about';
 import Nav from './component/layout/Nav';
 import './App.css'
 
@@ -8,13 +5,10 @@ import './App.css'
 function App() {
   return (
       <div className='App'>
-       <Nav/>
-         <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
      
-      
+        <Nav/>
+ 
+   
       </div>
 
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import Home from './component/Home'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <BrowserRouter>
-    <App/>
 
-   </BrowserRouter>
-   
+  <HashRouter>
+  <Routes>
+        <Route path="/" element={<App />} />
+        <Route index element={<Home />} />
+      </Routes>
+  </HashRouter>
+  
   </React.StrictMode>,
 )
