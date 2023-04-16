e0883e907b3095277692fb0edf9a9e90960ba209
johandry8919 <rojas.rojas440@gmail.com>
2023-04-16 03:41:45 -0400

    commit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,80 +1,145 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import Footter from './layout/Footter';
+import logo1 from '../assets/img/fond.png';
+import logo2 from '../assets/img/logo2.png';
+import logo3 from '../assets/img/El movidas.png';
 
-import logo2 from '../assets/img/logo2.png'
-import MyButton from '../component/MyButton/MyButton'
-import '../component/header/Header.css'
-import 'animate.css/animate.min.css';import 'animate.css/animate.min.css';
-import Footter from './layout/Footter'
+import ReCAPTCHA from 'react-google-recaptcha';
 
 
 
 function Home() {
-    const enviar = 'Enviar'
-    return (  
 
-        <div className=' m-auto mt-4   Headers'>
+  const [correo, setCorreo] = useState('');
+  const [mensaje, setMensaje] = useState('');
+  const [nombre, setNombre] = useState('');
+  const [isVerified, setIsVerified] = useState(false);
 
 
 
+    const [images, setImages] = useState([logo1, logo2, logo3]);
+    const [currentImage, setCurrentImage] = useState(0);
 
-            <section className='card container text-uppercase'>
 
-            <div className='card-header text-center'>
-                <h1>Contactame  disponible la 24hr</h1>
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImage((currentImage + 1) % images.length);
+    }, 5000);
+    return () => clearInterval(intervalId);
+  }, [images, currentImage]);
+
+  const handleRecaptcha = (response) => {
+    if (response) {
+      setIsVerified(true);
+    }
+  }
+
+
+
+  const enviarMensaje = () => {
+    const mensaje2 = `Nuevo mensaje de ${correo} nombre: ${nombre} Menaje: ${mensaje}`;
+    const TOKEN = '6158377987:AAESF42hcjU7uLYRjGjGi-rsjMtPuLq_Igw'
+    const CHAT_ID = '6021886971';
+    fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${mensaje2}`)
+      .then(response => {
+        // Muestra la alerta de éxito
+        const alerta = document.getElementById('alerta');
+        alerta.classList.remove('d-none');
+        alerta.style.opacity = 1;
+
+        // Oculta la alerta después de 5 segundos
+        setTimeout(() => {
+        alerta.style.opacity = 0;
+        setTimeout(() => {
+            alerta.classList.add('d-none');
+            setCorreo('');
+             setMensaje('');
+            
+             location.reload()
+            
+        }, 500);
+        }, 2000);
+
+            })
+      .catch(error => console.error(error));
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+      if (isVerified) {
+        // Realiza la acción de envío del formulario
+        enviarMensaje();
+      } else {
+        // Muestra un mensaje de error al usuario
+        alert('Por favor, verifica que eres un humano');
+      }
+  };
+
+
+
+  return (  
+    <div className=' m-auto mt-4 Headers'>
+
+                <div id="alerta" class="alert alert-success d-none text-center" role="alert">
+                Mensaje enviado correctamente.
+                </div>
+      <section className='card container text-uppercase'>
+        <div className='card-header text-center'>
+          <h1 className="p-0 m-0">CONTACTAME DISPONIBLE LA 24 HR</h1>
+        </div>
+        <div className="p-2">
+          <div className="row justify-content-center">
+          <div className="col-12 col-md-6">
+            <img
+                className={`img-fluid animate__animated animate__backInLeft ${currentImage === 2 ? 'active' : ''} transition-img`}
+                src={images[currentImage]}
+                alt="logo"
+            />
+            </div>
+            <div className="col-12 col-md-6 textareas animate__animated animate__bounce">
+            <form onSubmit={handleSubmit}>
+                    <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Tu correo</label>
+                    <input  required type="email"
+                        value={correo}
+                        onChange={e => setCorreo(e.target.value)}  class="form-control" id="exampleFormControlInput1" placeholder="correo"/>
+                    </div>
+
+                    <div class="mb-3">
+                    <label for="" class="form-label">Nombre</label>
+                    <input  required type="text"
+                        value={nombre}
+                        onChange={e => setNombre(e.target.value)}  class="form-control" id="" placeholder="Escribe tu nombre"/>
+                    </div>
+                    <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Contactame</label>
+                    <textarea required value={mensaje}
+                    onChange={e => setMensaje(e.target.value)} class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+
+                    <ReCAPTCHA
+                    sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+                    onChange={handleRecaptcha}
+                    className='text-center p-3 m-auto'
+      />
+                    <button className='btn btn-primary w-100' type="submit">Enviar</button>
+
+             </form>
+
 
             </div>
-                <div className="p-2">
-                    
-                       <div className="row  justify-content-center ">
+          </div>
+        </div>
+        <hr />
+        <div className='footter mt-5'>
+          <Footter/>
+        </div>
+      </section>
+    </div>
+  );
 
-
-                        <div className="col-12 col-md-6">
-                            <img
-                              className="img-fluid animate__animated animate__backInLeft transition-img"
-                              src={logo2}
-                              alt="logo"
-                            />
-                          </div>
-
-
-                        <div className="col-12 col-md-6 textareas animate__animated  animate__bounce">
-                            <form action="">
-                                <div className="mb-3">
-                                    <label for="exampleFormControlInput1" className="form-label">Correo Electronico</label>
-                                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-                                  </div>
-                                  <div className="mb-3">
-                                    <label for="exampleFormControlTextarea1" className="form-label">Enviame un messaje</label>
-                                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                                  </div>
-
-                                  <MyButton MyButtons= {enviar}/>
-                              
-                            </form>
-
-                         
-                        </div>
-
-                       </div>
-                        
-                </div>
-
-            <div className='footter mt-5 '>
-                <Footter/>
-           </div>
-
-
-            </section>
-
-           
-
-           
-
-
-            
-         
-         </div>
-    );
+  
 }
 
 export default Home;
--- src/component/layout/Footter.jsx
+++ src/component/layout/Footter.jsx
@@ -1,13 +1,38 @@
-import fondo from '../../assets/img/programador full starck.png'
+import fondo from '../../assets/img/vedeis.mp4'
 
 function Footter() {
+
+    const styles = {
+        position: 'relative',
+        width: '100%',
+        height: 0,
+        paddingTop: '33.3333%',
+        paddingBottom: 0,
+        boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)',
+        marginTop: '1.6em',
+        marginBottom: '0.9em',
+        overflow: 'hidden',
+        borderRadius: '8px',
+        willChange: 'transform'
+      };
+      
+      const srcUrl = encodeURIComponent('https://www.canva.com/design/DAFgORdFfP8/watch?embed');
+      
     return ( 
 
         <div class="footer">
-            <footer className="bg-black text-white ">
-               <img class="w-100" src={fondo} alt=""/>
+
+            <div style={styles}>
+                <iframe loading="lazy" style={{ position: 'absolute', width: '100%', height: '100%', top: 0, left: 0, border: 'none', padding: 0, margin: 0 }}
+                src={decodeURIComponent(srcUrl)} allowFullScreen={true} allow="fullscreen" autoplay>
+                </iframe>
+            </div>
+                    
+
+            <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFgORdFfP8&#x2F;watch?utm_content=DAFgORdFfP8&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">programador full starck</a> de johandry
+
         
-                </footer>
+              
         </div>
      );
 }
